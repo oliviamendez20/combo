a4ee196bae0a502f023a51bfa0f4f82509161ab8
Olivia Mendez <olivianevaeh@yahoo.com>
2024-04-02 09:14:08 -0500

    Initial Commit

---
+++ comboTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  comboTutorial
+//
+//  Created by OLIVIA MENDEZ on 4/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ comboTutorial/comboTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  comboTutorialApp.swift
+//  comboTutorial
+//
+//  Created by OLIVIA MENDEZ on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct comboTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
